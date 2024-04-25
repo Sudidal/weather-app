25595423f6734397a1d14d1f6fed03ef08fc1141
Oxide <mohammedhaider15mo@gmail.com>
2024-04-25 17:48:22 +0300

    Add more weather info for the page and grid layout setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,55 @@
+import "./visual.js";
+import "./style.css";
 import getData from "./weatherRequest.js";
 import { setWeatherUI } from "./visual.js";
+
+const hoursAhead = 6;
 
 function getWeatherData(city, lang) {
   getData(city, lang, onDataReceive);
 }
 
 function onDataReceive(data) {
-  console.log(data.forecast.forecastday[0].day.avgtemp_c);
   const conimg = data.current.condition.icon;
   const contxt = data.current.condition.text;
-  const temp = data.current.temp_c;
+  const location = data.location.name;
+  const day = data.forecast.forecastday[0].date;
+  const avgtemp = data.forecast.forecastday[0].day.avgtemp_c;
+  const maxtemp = data.forecast.forecastday[0].day.maxtemp_c;
+  const mintemp = data.forecast.forecastday[0].day.mintemp_c;
   const humidity = data.current.humidity;
   const windSpeed = data.current.wind_kph;
-  setWeatherUI(conimg, contxt, temp, humidity, windSpeed);
+  const persipitation = data.forecast.forecastday[0].day.daily_chance_of_rain;
+
+  const hours = Array(6);
+  for (let i = 0; i < hoursAhead; i++) {
+    const hour = i;
+    const img = data.forecast.forecastday[0].hour[i].condition.icon;
+    const temp = data.forecast.forecastday[0].hour[i].temp_c;
+    const obj = {
+      hour,
+      img,
+      temp,
+    };
+    hours[i] = obj;
+  }
+  console.log(hours);
+  setWeatherUI(
+    conimg,
+    contxt,
+    location,
+    day,
+    avgtemp,
+    maxtemp,
+    mintemp,
+    humidity,
+    windSpeed,
+    persipitation,
+    hours,
+  );
 }
+
+// console.log("debuggg here")
+getWeatherData("hilla iraq", "en");
 
 export { getWeatherData };
--- src/visual.js
+++ src/visual.js
@@ -6,9 +6,17 @@ const langInput = document.querySelector(".lang-input");
 
 const conditionIcon = document.querySelector(".condition-icon");
 const conditionText = document.querySelector(".condition-text");
-const tempText = document.querySelector(".temp-text");
+const locationText = document.querySelector(".location-text");
+const dayText = document.querySelector(".day-text");
+
+const avgTempText = document.querySelector(".avg-temp-text");
+const maxTempText = document.querySelector(".max-temp-text");
+const minTempText = document.querySelector(".min-temp-text");
 const humidityText = document.querySelector(".humidity-text");
 const windSpeedText = document.querySelector(".wind-speed-text");
+const percipitationText = document.querySelector(".persipitation-text");
+
+const hoursList = document.querySelector(".hours-list");
 
 getInfoBtn.addEventListener("click", () => {
   const _city = cityInput.value;
@@ -16,12 +24,36 @@ getInfoBtn.addEventListener("click", () => {
   getWeatherData(_city, _lang);
 });
 
-function setWeatherUI(conimg, contxt, temp, humidity, windSpeed) {
+function setWeatherUI(
+  conimg,
+  contxt,
+  location,
+  day,
+  avgtemp,
+  maxtemp,
+  mintemp,
+  humidity,
+  windSpeed,
+  persipitation,
+  hours,
+) {
   conditionIcon.src = conimg;
   conditionText.textContent = contxt;
-  tempText.textContent = temp + "c°";
-  humidityText.textContent = humidity + "%";
+  locationText.textContent = location;
+  dayText.textContent = day;
+  avgTempText.textContent = avgtemp + "c°";
+  maxTempText.textContent = maxtemp + "c°";
+  minTempText.textContent = mintemp + "c°";
+  humidityText.textContent = "humidity " + humidity + "%";
   windSpeedText.textContent = windSpeed + "kph";
+  percipitationText.textContent = "persipitation " + persipitation + "%";
+
+  for (let i = 0; i < hours.length; i++) {
+    const element = hoursList.children[i];
+    element.querySelector(".time").textContent = hours[i].hour;
+    element.querySelector(".temp").textContent = hours[i].temp;
+    element.querySelector("img").src = hours[i].img;
+  }
 }
 
 export { setWeatherUI };
